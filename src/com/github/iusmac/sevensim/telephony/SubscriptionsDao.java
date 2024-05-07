faa5eb958b03100d8ff663adf825918153f0f0bf
iusmac <iusico.maxim@libero.it>
2024-05-07 17:41:29 +0200

    refactor(telephony): drop unused CRUD operations from `SubscriptionsDao`
    They were stripped off by Proguard anyway, but we can speed up the build
    by avoiding to generate them.
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- src/com/github/iusmac/sevensim/telephony/SubscriptionsDao.java
+++ src/com/github/iusmac/sevensim/telephony/SubscriptionsDao.java
@@ -1,27 +1,15 @@
 package com.github.iusmac.sevensim.telephony;
 
 import androidx.room.Dao;
-import androidx.room.Delete;
-import androidx.room.Insert;
 import androidx.room.Query;
-import androidx.room.Update;
 import androidx.room.Upsert;
 
 import java.util.Optional;
 
 @Dao
 public interface SubscriptionsDao {
-    @Insert
-    long insert(Subscription subscription);
-
-    @Update
-    int update(Subscription subscription);
-
     @Upsert
     void upsert(final Subscription subscription);
-
-    @Delete
-    void delete(Subscription subscription);
 
     @Query("SELECT * FROM subscriptions WHERE id = :subId")
     Optional<Subscription> findBySubscriptionId(int subId);
