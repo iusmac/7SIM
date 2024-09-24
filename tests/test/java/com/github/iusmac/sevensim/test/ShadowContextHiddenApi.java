93018520a1c99797f639a7c512798ec4c38cd514
iusmac <iusico.maxim@libero.it>
2024-09-24 18:44:14 +0200

    Unit tests for `UserAuthenticationObserverService` class

---
+++ tests/test/java/com/github/iusmac/sevensim/test/ShadowContextHiddenApi.java
@@ -0,0 +1,19 @@
+package com.github.iusmac.sevensim.test;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.UserHandle;
+
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+import org.robolectric.shadows.ShadowContextImpl;
+
+import static android.os.Build.VERSION_CODES.O;
+
+@Implements(className = ShadowContextImpl.CLASS_NAME)
+public class ShadowContextHiddenApi extends ShadowContextImpl {
+    @Implementation(minSdk = O)
+    protected ComponentName startForegroundServiceAsUser(Intent service, UserHandle user) {
+        return startForegroundService(service);
+    }
+}
