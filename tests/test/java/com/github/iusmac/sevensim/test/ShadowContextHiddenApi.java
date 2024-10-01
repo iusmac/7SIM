ce99993ce97a57757ec0e7a902673615bb0f3762
iusmac <iusico.maxim@libero.it>
2024-10-01 10:53:01 +0200

    Unit tests for `LauncherIconVisibilityManager` class

--- tests/test/java/com/github/iusmac/sevensim/test/ShadowContextHiddenApi.java
+++ tests/test/java/com/github/iusmac/sevensim/test/ShadowContextHiddenApi.java
@@ -8,6 +8,7 @@ import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 import org.robolectric.shadows.ShadowContextImpl;
 
+import static android.os.Build.VERSION_CODES.KITKAT;
 import static android.os.Build.VERSION_CODES.O;
 
 @Implements(className = ShadowContextImpl.CLASS_NAME)
@@ -15,5 +16,15 @@ public class ShadowContextHiddenApi extends ShadowContextImpl {
     @Implementation(minSdk = O)
     protected ComponentName startForegroundServiceAsUser(Intent service, UserHandle user) {
         return startForegroundService(service);
+    }
+
+    @Implementation(minSdk = KITKAT)
+    protected ComponentName startServiceAsUser(Intent service, UserHandle user) {
+        return startService(service);
+    }
+
+    @Implementation(minSdk = KITKAT)
+    protected boolean stopServiceAsUser(Intent service, UserHandle user) {
+        return stopService(service);
     }
 }
