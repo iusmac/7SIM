e91a671d129b83f0fb57935c8839bcc41669d165
iusmac <iusico.maxim@libero.it>
2025-06-30 18:08:20 +0200

    Subscriptions: fix `UnnecessaryQualifier` warning
    /home/runner/work/7SIM/7SIM/src/com/github/iusmac/sevensim/telephony/Subscriptions.java:125: warning: [UnnecessaryQualifier] A qualifier annotation has no effect here.
        Subscriptions(final @ApplicationContext Context context,
                            ^
        (see https://errorprone.info/bugpattern/UnnecessaryQualifier)
      Did you mean 'Subscriptions(final  Context context,'?
    /home/runner/work/7SIM/7SIM/src/com/github/iusmac/sevensim/telephony/Subscriptions.java:128: warning: [UnnecessaryQualifier] A qualifier annotation has no effect here.
                final @Named("Telephony/SubState") SysProp subStateSysProp,
                      ^
        (see https://errorprone.info/bugpattern/UnnecessaryQualifier)
      Did you mean 'final  SysProp subStateSysProp,'?
    /home/runner/work/7SIM/7SIM/src/com/github/iusmac/sevensim/telephony/Subscriptions.java:129: warning: [UnnecessaryQualifier] A qualifier annotation has no effect here.
                final @Named("Telephony/UsableSubIds") SysProp usableSimSubIdsSysProp) {
                      ^
        (see https://errorprone.info/bugpattern/UnnecessaryQualifier)
      Did you mean 'final  SysProp usableSimSubIdsSysProp) {'?

--- src/com/github/iusmac/sevensim/telephony/Subscriptions.java
+++ src/com/github/iusmac/sevensim/telephony/Subscriptions.java
@@ -22,8 +22,6 @@ import com.github.iusmac.sevensim.Logger;
 import com.github.iusmac.sevensim.SysProp;
 import com.github.iusmac.sevensim.Utils;
 
-import dagger.hilt.android.qualifiers.ApplicationContext;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -34,8 +32,6 @@ import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Predicate;
-
-import javax.inject.Named;
 
 import static com.github.iusmac.sevensim.telephony.Subscription.DEFAULT_SIM_STATE;
 
@@ -123,11 +119,11 @@ public abstract class Subscriptions implements Iterable<Subscription> {
     private final SysProp mUsableSubIdsSysProp;
     final SubscriptionsDao mSubscriptionsDao;
 
-    Subscriptions(final @ApplicationContext Context context,
+    Subscriptions(final Context context,
             final Logger.Factory loggerFactory, final AppDatabaseDE appDatabase,
             final SubscriptionManager subscriptionManager,
-            final @Named("Telephony/SubState") SysProp subStateSysProp,
-            final @Named("Telephony/UsableSubIds") SysProp usableSimSubIdsSysProp) {
+            final SysProp subStateSysProp,
+            final SysProp usableSimSubIdsSysProp) {
 
         mContext = context;
         mLogger = loggerFactory.create(getClass().getSimpleName());
